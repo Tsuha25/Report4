089b51c31cea3ae4ecad98f8bfad32d75cb0cb15
Syoya Tsuha <e185735@ie.u-ryukyu.ac.jp>
2018-11-25 21:12:58 +0900

    Report4 Second Commit

--- src/Main/Step1.java
+++ src/Main/Step1.java
@@ -1,7 +1,12 @@
 public class Step1 {
 
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+        System.out.println("NullPointerExceptionが発生しました。");
+        System.out.println(e.getMessage());
+        }
     }
 }
